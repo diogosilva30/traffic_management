43732fb3626b6951f8aed536df847d249a3cce1c
spamz23 <diogosilv30@gmail.com>
2022-05-29 16:30:51 +0100

    Finish 'characterization' filter

--- roads/filters.py
+++ roads/filters.py
@@ -28,20 +28,31 @@ class CharacterizationFilter(filters.FilterSet):
         First we find the most recent `SpeedReading` of each `RoadSegment`,
         then we filter based on `characterization`.
         """
+        from rest_framework import exceptions
         from django.db.models import OuterRef, Subquery
 
+        # Validate 'characterization' value
+        acceptable_values = ["low", "moderate", "high"]
+        if value not in acceptable_values:
+            raise exceptions.ValidationError(
+                {
+                    name: f"'{value}' is not a valid option. Please use of the following: {acceptable_values}"
+                }
+            )
         newest = SpeedReading.objects.filter(road_segment=OuterRef("pk")).order_by(
             "-timestamp"
         )
-        segments = RoadSegment.objects.annotate(
+        # First filter road segments without speed readings, then
+        # annotate objects with the id of the latest reading
+        segments = RoadSegment.objects.exclude(speed_readings__isnull=True).annotate(
             newest_speed_id=Subquery(newest.values("id")[:1]),
         )
 
-        [s for s in segments]
-        print(segments[200].newest_speed_id)
+        segments_ids = [
+            s.id for s in segments if s.newest_speed_id__characterization == value
+        ]
 
-        raise ValueError
-        # return queryset.filter(… some filtering …)
+        return queryset.filter(pk__in=segments_ids)
 
     class Meta:
         model = RoadSegment
